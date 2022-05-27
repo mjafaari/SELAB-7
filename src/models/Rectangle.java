181449bfaf1019b6cad83a30d064074393707e53
mjafaari <mjafaari99@gmail.com>
2022-05-27 22:43:56 +0430

    Rectangle getter and setter methods added and tested

--- src/models/Rectangle.java
+++ src/models/Rectangle.java
@@ -13,4 +13,20 @@ public class Rectangle extends Figure{
     public double computeArea() {
         return width * length;
     }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
 }
