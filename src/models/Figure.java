ad798ccdd5219206179682708e38cf03a66b5dbb
mjafaari <mjafaari99@gmail.com>
2022-05-27 22:41:12 +0430

    Rectangle model added

---
+++ src/models/Figure.java
@@ -0,0 +1,5 @@
+package models;
+
+abstract class Figure {
+    public abstract double computeArea();
+}
---
+++ src/models/Rectangle.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Rectangle extends Figure{
+    private double length;
+    private double width;
+
+    public Rectangle(double l, double w) {
+
+    }
+
+    @Override
+    public double computeArea() {
+        return 1;
+    }
+}
